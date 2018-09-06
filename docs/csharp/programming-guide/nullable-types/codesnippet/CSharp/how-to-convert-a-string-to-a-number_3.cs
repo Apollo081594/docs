cc138722a43262d472962c96600ef18ce34de149
Christian Rey Baron <32894467+Apollo081594@users.noreply.github.com>
2018-09-06 15:24:41 +0800

    Remove declaration of int j on top of statement
    Remove int j and move it next in "out"

--- docs/csharp/programming-guide/nullable-types/codesnippet/CSharp/how-to-convert-a-string-to-a-number_3.cs
+++ docs/csharp/programming-guide/nullable-types/codesnippet/CSharp/how-to-convert-a-string-to-a-number_3.cs
@@ -1,7 +1,7 @@
             // TryParse returns true if the conversion succeeded
             // and stores the result in j.
-            int j;
-            if (Int32.TryParse("-105", out j))
+        
+            if (Int32.TryParse("-105", out int j))
                 Console.WriteLine(j);
             else
                 Console.WriteLine("String could not be parsed.");
